abcaae7f0838a03e23cc86bd32758f5abc098a6d
Andreas Thienemann <andreas@bawue.net>
2020-07-19 13:28:57 +0200

    Bump release to 0.6.0

--- setup.py
+++ setup.py
@@ -3,18 +3,27 @@ import uuid
 
 from setuptools import setup, find_packages
 try:
+    # pip >=20
+    from pip._internal.network.session import PipSession
     from pip._internal.req import parse_requirements
 except ImportError:
-    from pip.req import parse_requirements
+    try:
+        # 10.0.0 <= pip <= 19.3.1
+        from pip._internal.download import PipSession
+        from pip._internal.req import parse_requirements
+    except ImportError:
+        # pip <= 9.0.3
+        from pip.download import PipSession
+        from pip.req import parse_requirements
 
 __author__ = 'Andreas Thienemann <andreas@bawue.net>'
 
 install_reqs = parse_requirements('requirements.txt', session=uuid.uuid1())
-reqs = [str(ir.requirement) for ir in install_reqs]
+reqs = [str(ir.req) for ir in install_reqs]
 
 setup(
     name="napalm-procurve",
-    version="0.5.0",
+    version="0.6.0",
     packages=find_packages(),
     author="Andreas Thienemann",
     author_email="andreas@bawue.net",
